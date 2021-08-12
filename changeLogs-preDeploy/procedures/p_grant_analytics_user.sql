1364961b863182ab204278351740ac4d7fca2aff
Oleksii Baranov <oleksii_baranov@epam.com>
2021-08-12 13:26:16 +0300

    [EDP-DDM-8475] quick fix on grants to analytics_officer role

--- changeLogs-preDeploy/procedures/p_grant_analytics_user.sql
+++ changeLogs-preDeploy/procedures/p_grant_analytics_user.sql
@@ -4,7 +4,20 @@ AS $procedure$
 DECLARE
   c_obj_pattern TEXT := 'report%v';
   r RECORD;
+  is_role_found integer;
 BEGIN
+
+  -- check if role exists
+  select 1
+    into is_role_found
+    from pg_catalog.pg_roles
+    where rolname = p_user_name;
+
+  if is_role_found is null then
+    return;
+  end if;
+
+
   FOR r IN SELECT * FROM information_schema.views WHERE table_name LIKE c_obj_pattern AND table_schema = 'public' LOOP
     EXECUTE 'GRANT SELECT ON ' || r.table_name || ' TO ' || p_user_name || ';';
   END LOOP;
